f488d6d3eff1c671a090774c0a2e542f11bfe1c1
vauxhall <zero.griffin@gmail.com>
2020-07-03 16:46:35 +0200

    Add compare options

--- iOS/Tint.swift
+++ iOS/Tint.swift
@@ -5,6 +5,8 @@ struct Tint: View {
     let shades: [Shade]
     @State private var compare = Shade.clear
     @State private var background = Shade.clear
+    @State private var sheetCompare = false
+    @State private var sheetBackground = false
     
     var body: some View {
         ZStack {
@@ -30,30 +32,46 @@ struct Tint: View {
                 HStack {
                     Spacer()
                     Control(title: "Compare", action: {
-                        
+                        self.sheetCompare = true
                     }).padding()
-                    Button(action: {
-                        
-                    }) {
-                        Text("Background")
-                    }.padding()
+                    Control(title: "Background", action: {
+                        self.sheetBackground = true
+                    }).padding()
                     Spacer()
                 }.padding(.bottom, 10)
             }
         }.navigationBarTitle(.init(verbatim: shade.name), displayMode: .inline)
+            .sheet(isPresented: .init(get: {
+                self.sheetBackground || self.sheetCompare
+            }, set: { _ in
+                self.sheetBackground = false
+                self.sheetCompare = false
+            })) {
+                Sheet(title: "Compare", current: self.sheetCompare ? self.compare : self.background, shades: self.shades) {
+                    if self.sheetCompare {
+                        self.compare = $0
+                    }
+                    if self.sheetBackground {
+                        self.background = $0
+                    }
+                    self.sheetCompare = false
+                    self.sheetBackground = false
+                }
+            }
     }
 }
 
 private struct Control: View {
-    var title: LocalizedStringKey
-    var action: () -> Void
+    let title: LocalizedStringKey
+    let action: () -> Void
     
     var body: some View {
         Button(action: action) {
             ZStack {
                 RoundedRectangle(cornerRadius: 22)
                     .frame(width: 120, height: 44)
-                    .shadow(color: .secondary, radius: 5, x: 0, y: 0)
+                    .shadow(color: .secondary, radius: 2, x: -2, y: -2)
+                    .shadow(color: .init(.systemBackground), radius: 2, x: 2, y: 2)
                     .foregroundColor(.init(.systemBackground))
                 Text(title)
                     .foregroundColor(.primary)
@@ -61,5 +79,45 @@ private struct Control: View {
                     .bold()
             }
         }
+    }
+}
+
+private struct Sheet: View {
+    let title: LocalizedStringKey
+    let current: Shade
+    let shades: [Shade]
+    let action: (Shade) -> Void
+    
+    var body: some View {
+        List {
+            Section(header:
+            Text(title)
+                .font(.headline)
+                .padding(.vertical)) {
+                ForEach([Shade.clear] + shades) { shade in
+                    Button(action: {
+                        self.action(shade)
+                    }) {
+                        HStack {
+                            Rectangle()
+                                .cornerRadius(8)
+                                .foregroundColor(shade.color)
+                                .frame(width: 50)
+                            Text(shade.name)
+                                .padding(.vertical)
+                            Spacer()
+                            
+                            if shade.id == self.current.id {
+                                Image(systemName: "checkmark.circle.fill")
+                                    .resizable()
+                                    .frame(width: 22, height: 22)
+                                    .foregroundColor(.blue)
+                                    .padding(.vertical)
+                            }
+                        }
+                    }
+                }
+            }
+        }.listStyle(PlainListStyle())
     }
 }
